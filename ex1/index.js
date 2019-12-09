5309a9d3b826528f80abdc84750a86a880c1786f
Nikita Jiandani <niki.jiandani@gmail.com>
2019-12-08 23:07:01 -0500

    Add solution to Ex1 part 2

--- ex1/index.js
+++ ex1/index.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
+
 const input = fs
   .readFileSync('./input.txt')
   .toString()
   .trim()
   .split('\n');
+
+function fuelRequired(mass) {
+  return Math.floor(mass / 3) - 2;
+}
+
 const result = input
   .map(x => parseInt(x))
   .reduce((a, b) => {
-    return a + (Math.floor(b / 3) - 2);
+    let currentFuel = fuelRequired(b);
+    let totalFuel = 0;
+    while (currentFuel > 0) {
+      totalFuel += currentFuel;
+      currentFuel = fuelRequired(currentFuel);
+    }
+    return a + totalFuel;
   }, 0);
 console.log(result);
